1080148612a9dc52198bc6693a56b3809ed874b4
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-10-14 13:50:48 +0200

    minor bug in signup.js (API)

--- routes/API/signup.js
+++ routes/API/signup.js
@@ -1,4 +1,4 @@
-const db = require('../models/index');
+const db = require('../../models/index');
 const ash = require('express-async-handler');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
