6f9795696ce5997dba424b51264871235e7f9c1a
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-02-07 18:48:58 +0100

    added sign user controller and router

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,6 +31,22 @@ const createSchema = Joi.object({
 })
   .with('password', 'confirmPassword');
 
+const signSchema = Joi.object({
+  email: Joi.string()
+    .min(5)
+    .email({
+      minDomainSegments: 2,
+      tlds: {
+        allow: ['com', 'fr', 'net']
+      }
+    })
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .max(20)
+    .required()
+})
+
 exports.create = ash(async function(req, res) {
   console.log(req.body); // debug
 
@@ -55,15 +71,15 @@ exports.create = ash(async function(req, res) {
   const userName = req.body.name;
   var user = await db.User.findOne({
     where: {
-      name: req.body.name
+      name: userName
     }
   });
 
   if (user !== null) {
     res.status(500).send({
-      error: `User ${userName} already exists.`
+      error: `User name=${userName} already exists.`
     });
-    // return;
+    return;
   }
 
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -85,6 +101,73 @@ exports.create = ash(async function(req, res) {
 
   res.status(200).send({
     message: 'User created successfully!'
+  });
+  return;
+});
+
+exports.sign = ash(async function(req, res) {
+  console.log(req.body); // debug
+
+  const requestKeys = Object.keys(req.body);
+
+  if (requestKeys.length === 0) {
+    res.status(400).send({
+      error: 'Fields cannot be empty.'
+    });
+    return;
+  }
+
+  const formValidation = signSchema.validate(req.body);
+
+  if (formValidation.error) {
+    res.status(400).send({
+      error: formValidation.error.details[0].message
+    });
+    return;
+  }
+
+  const userEmail = { email: req.body.email };
+  var user = await db.User.findOne({
+    where: userEmail
+  });
+
+  if (user === null) {
+    res.status(500).send({
+      error: `User with email=${userEmail} doesn't exist.`
+    });
+    return;
+  }
+
+  const passwordValidation = await bcrypt.compare(req.body.password, user.password);
+
+  if (!passwordValidation) {
+    res.status(400).send({
+      error: 'Invalid password.'
+    });
+    return;
+  }
+
+  const token = {};
+  var date = new Date(Date.now());
+  date.setHours(date.getHours() + 1)
+
+  token[req.session.id] = date;
+
+  user = await db.User.update({
+    token: JSON.stringify(token)
+  }, {
+    where: userEmail
+  });
+
+  if (user != 1) {
+    res.status(500).send({
+      error: `Cannot sign in user with email=${req.body.email}. Maybe user was not found or fields are empty.`
+    });
+    return;
+  }
+
+  res.status(200).send({
+    message: 'User signed successfully!'
   });
   return;
 });
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 
 router.post('/', userController.create);
+router.put('/sign', userController.sign);
 
 module.exports = router;
