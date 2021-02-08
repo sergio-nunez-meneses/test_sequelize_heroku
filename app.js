5dae9910b196a7bd89c02b84d9877db8d50f381e
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-02-09 00:31:54 +0100

    updated controllers and routes directories, and app.js file

--- app.js
+++ app.js
@@ -5,24 +5,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-var userRouter = require('./routes/user');
-var signinRouter = require('./routes/signin'); // site home
-var signupRouter = require('./routes/signup');
-var logoutRouter = require('./routes/logout');
-var displayRouter = require('./routes/displayFarmers'); // admin home
-var insertRouter = require('./routes/insertFarmer');
-var editRouter = require('./routes/editFarmer');
-var deleteRouter = require('./routes/deleteFarmer');
-// API
-var fetchRouter = require('./routes/fetch');
-var apiDisplayRouter = require('./routes/API/displayAll');
-var apiRouter = require('./routes/API/search');
-var apiSignUpRouter = require('./routes/API/signup');
-var apiSignInRouter = require('./routes/API/signin');
-var apiInsertRouter = require('./routes/API/insertFarmer');
-var apiEditRouter = require('./routes/API/editFarmer');
-var apiDeleteRouter = require('./routes/API/deleteFarmer');
-
 var app = express();
 
 // sessions
@@ -70,26 +52,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin/users', require('./routes/userRoute'));
 app.use('/api/farmers', require('./routes/farmerRoute'));
 app.use('/api/farms', require('./routes/farmRoute'));
-
-// autoload ? fs module
-app.use('/', signinRouter);
-app.use('/user', userRouter);
-app.use('/signin', signinRouter);
-app.use('/signup', signupRouter);
-app.use('/logout', logoutRouter);
-app.use('/displayFarmers', displayRouter);
-app.use('/insertFarmer', insertRouter);
-app.use('/editFarmer', editRouter);
-app.use('/deleteFarmer', deleteRouter);
-// API
-app.use('/fetch', fetchRouter);
-app.use('/API/displayAll', apiDisplayRouter);
-app.use('/API/search', apiRouter);
-app.use('/API/signup', apiSignUpRouter);
-app.use('/API/signin', apiSignInRouter);
-app.use('/API/insertFarmer', apiInsertRouter);
-app.use('/API/editFarmer', apiEditRouter);
-app.use('/API/deleteFarmer', apiDeleteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/logout.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const logoutController = require('../controllers/logout');
-
-router.all('/', logoutController.logout);
-
-module.exports = router;
