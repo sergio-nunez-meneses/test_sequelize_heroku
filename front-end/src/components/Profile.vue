470214bb9ca8158619f62f6d133b50d0144a39ee
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-02-16 16:24:35 +0100

    front-end: added farmers list component and farmers service; updated all components and router.js file

--- front-end/src/components/Profile.vue
+++ front-end/src/components/Profile.vue
@@ -7,31 +7,45 @@
     </div>
     <div class="row">
       <div class="col-md-12">
-        <div class="my-auto p-3 text-center">
-          <ul class="list-group">
-            <li class="list-group-item">
-              {{ currentUser.accessToken.substring(0, 20) }}...{{ currentUser.accessToken.substr(currentUser.accessToken.length - 20) }}
-            </li>
-            <li class="list-group-item"> {{ currentUser.id }} </li>
-            <li class="list-group-item"> {{ currentUser.email }} </li>
-            <li class="list-group-item"> {{ currentUser.role }} </li>
-          </ul>
-        </div>
-        <div v-if=" currentUser.role === 'Admin' " class="p-3">
-          <router-link
-            v-for="(endpoint, index) in allowedEndpoints"
-            :key="index"
-            :to="endpoint"
-            class="btn w-100 my-1 bg-primary text-white"
-          >
-            {{
-              endpoint
-                .substring(1)
-                .charAt(0)
-                .toUpperCase()
-                .concat(endpoint.substring(2))
-            }} List
-          </router-link>
+        <div class="w-75 m-auto">
+          <div class="my-auto p-3 text-center">
+            <ul class="list-group">
+              <li class="list-group-item">
+                {{ currentUser.accessToken.substring(0, 20) }}...{{ currentUser.accessToken.substr(currentUser.accessToken.length - 20) }}
+              </li>
+              <li class="list-group-item"
+                @click="editOnClick(currentUser.id)"
+              >
+                {{ currentUser.id }}
+              </li>
+              <li class="list-group-item"
+                @click="editOnClick(currentUser.email)"
+              >
+                {{ currentUser.email }}
+              </li>
+              <li class="list-group-item"
+                @click="editOnClick(currentUser.role)"
+              >
+                {{ currentUser.role }}
+              </li>
+            </ul>
+          </div>
+          <div v-if=" currentUser.role === 'Admin' " class="p-3">
+            <router-link
+              v-for="(endpoint, index) in allowedEndpoints"
+              :key="index"
+              :to="endpoint"
+              class="btn w-100 my-1 bg-primary text-white"
+            >
+              {{
+                endpoint
+                  .substring(1)
+                  .charAt(0)
+                  .toUpperCase()
+                  .concat(endpoint.substring(2))
+              }} List
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -49,6 +63,11 @@ export default {
   computed: {
     currentUser() {
       return this.$store.state.auth.user;
+    }
+  },
+  methods: {
+    editOnClick(input) {
+      alert(input);
     }
   },
   mounted() {
