50c23d17fdf6fae99f89e823a9e1cf0d5b81550b
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-02-19 20:37:11 +0100

    front-end: added updateOne method in main service, and showInput, hideInput and updateFarmer methods in FarmersList component

--- front-end/src/services/MainService.js
+++ front-end/src/services/MainService.js
@@ -9,6 +9,10 @@ class MainService {
     return axios.get(api_url + instance, headers);
   }
 
+  updateOne(instance, data) {
+    return axios.put(api_url + instance, data, headers);
+  }
+
   deleteAll(instance) {
     return axios.delete(api_url + instance, headers);
   }
