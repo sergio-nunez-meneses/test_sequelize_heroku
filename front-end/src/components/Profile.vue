4fcd141325cf180f515b466688855a4a655031e5
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-02-19 08:35:18 +0100

    front-end: added updateButton and updated editOnClick method in profile component

--- front-end/src/components/Profile.vue
+++ front-end/src/components/Profile.vue
@@ -9,26 +9,30 @@
       <div class="col-md-12">
         <div class="w-75 m-auto">
           <div class="my-auto p-3 text-center">
+            <form name="update"></form>
             <ul class="list-group">
               <li class="list-group-item">
                 {{ currentUser.accessToken.substring(0, 20) }}...{{ currentUser.accessToken.substr(currentUser.accessToken.length - 20) }}
               </li>
               <li class="list-group-item"
-                @click="editOnClick(currentUser.id)"
+                @click="editOnClick(currentUser.id, $event)"
               >
                 {{ currentUser.id }}
               </li>
               <li class="list-group-item"
-                @click="editOnClick(currentUser.email)"
+                @click="editOnClick(currentUser.email, $event)"
               >
                 {{ currentUser.email }}
               </li>
               <li class="list-group-item"
-                @click="editOnClick(currentUser.role)"
+                @click="editOnClick(currentUser.role, $event)"
               >
                 {{ currentUser.role }}
               </li>
             </ul>
+            <button ref="updateButton" class="btn d-none w-100 my-1 btn-warning">
+              Update
+            </button>
           </div>
           <div v-if=" currentUser.role === 'Admin' " class="p-3">
             <router-link
@@ -66,8 +70,9 @@ export default {
     }
   },
   methods: {
-    editOnClick(input) {
-      alert(input);
+    editOnClick(input, event) {
+      event.target.innerHTML = '<input form="update" class="form-control" type="text" value="' + input + '">';
+      this.$refs.updateButton.classList.remove('d-none');
     }
   },
   mounted() {
